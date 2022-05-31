492989a056972d2de5df567620fb1578feca3949
Guilherme <guilhaugusto05@gmail.com>
2022-05-30 23:19:24 -0300

    feat: create migrations for the models

---
+++ app/backend/src/database/migrations/20220531004306-create-match.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      homeTeam: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'home_team',
+        references: {
+          model: 'teams',
+          key: 'id',
+        }
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'home_team_goals',
+      },
+      awayTeam: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'away_team',
+        references: {
+          model: 'teams',
+          key: 'id',
+        }
+      },
+      awayTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'away_team_goals',
+      },
+      inProgress: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'in_progress',
+      },
+    });
+  },
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
