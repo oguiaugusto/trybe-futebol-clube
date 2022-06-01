bd1007eed200f674ba8fcbac1a35787b7127e2cd
Guilherme <guilhaugusto05@gmail.com>
2022-06-01 12:04:48 -0300

    refractor: move JWTUser test mock to another file

--- app/backend/src/tests/JWTUser.test.ts
+++ app/backend/src/tests/JWTUser.test.ts
@@ -3,31 +3,25 @@ import { JsonWebTokenError } from 'jsonwebtoken';
 import decode from 'jwt-decode';
 import { IUserDecoded, IUserPublic } from '../interfaces/user';
 import jwtUser from '../utils/JWTUser';
-
-const user: IUserPublic = {
-  id: 1,
-  username: 'user123',
-  role: 'user',
-  email: 'user@email.com',
-};
+import { usersMockPublic } from './mocks/user';
 
 describe('Test JWTUser', () => {
   it('sign should return a token with encrypted user information', () => {
-    const token = jwtUser.sign(user);
+    const token = jwtUser.sign(usersMockPublic[0]);
 
     expect(token).not.to.be.undefined;
     expect(typeof token === 'string').to.be.true;
 
     const decodedUser = decode<IUserDecoded>(token);
-    expect(decodedUser.data).to.be.eql(user);
+    expect(decodedUser.data).to.be.eql(usersMockPublic[0]);
   });
 
   it('verify should return the user from the token passed as parameter', () => {
-    const token = jwtUser.sign(user);
+    const token = jwtUser.sign(usersMockPublic[0]);
     const verifiedUser = jwtUser.verify(token);
 
     expect(verifiedUser).to.have.property('data');
-    expect(verifiedUser.data).to.be.eql(user);
+    expect(verifiedUser.data).to.be.eql(usersMockPublic[0]);
   });
 
   it('verify should throw an error if token is invalid', () => {
