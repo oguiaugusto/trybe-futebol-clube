b950c22a0b5331e5173ae152fd4677161a1742b6
Guilherme <guilhaugusto05@gmail.com>
2022-06-01 13:56:19 -0300

    feat: create new error messages

--- app/backend/src/schemas/Messages.ts
+++ app/backend/src/schemas/Messages.ts
@@ -1,6 +1,10 @@
 enum Messages {
+  InternalServerError = 'Internal Server Error',
   WrongCredentials = 'Incorrect email or password',
   FieldsNotFilled = 'All fields must be filled',
+  TokenNotFound = 'Token not found',
+  TokenInvalid = 'Invalid token',
+  NoTokenUser = 'Error finding token user',
 }
 
 export default Messages;
