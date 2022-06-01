b119d8565d19af587c1980c53c07153339f1c631
Guilherme <guilhaugusto05@gmail.com>
2022-06-01 12:57:24 -0300

    chore: create user mocks

---
+++ app/backend/src/tests/mocks/user.ts
@@ -0,0 +1,43 @@
+import { IUser } from '../../interfaces/user';
+
+const usersMockPublic = [
+  {
+    id: 1,
+    username: 'user123',
+    role: 'user',
+    email: 'user1@email.com',
+  },
+  { id: 2,
+    username: 'user456',
+    role: 'user',
+    email: 'user2@email.com',
+  },
+  { id: 3,
+    username: 'admin123',
+    role: 'admin',
+    email: 'admin@email.com',
+  },
+];
+
+const usersMock: IUser[] = [
+  {
+    ...usersMockPublic[0],
+    password: '$2a$10$lJ5Ssw1jXkzBeGlLvdDD/ef/9XJiShQk0klGppf5bIshdvPFL31Hu',
+  },
+  {
+    ...usersMockPublic[1],
+    password: '$2a$10$9eRTynJm2yyILhtLIbNe9ulFNoUqbMIBzFjhMn2FgujH.24SShmva',
+  },
+  {
+    ...usersMockPublic[2],
+    password: '$2a$10$nUqVP4.isLYchPUj3ko0HONUifxOHzX/NrGZnI//u3XI5IGPWtG4u',
+  },
+];
+
+const usersPassword = ['secret_user123', 'secret_user456', 'secret_admin123'];
+
+export {
+  usersMockPublic,
+  usersMock,
+  usersPassword,
+};
