19cfde72de8269426002ad0273836138f4142844
Guilherme <guilhaugusto05@gmail.com>
2022-06-01 13:56:52 -0300

    refractor: update ErrorMiddleware to handle JWT errors

--- app/backend/src/middlewares/ErrorMiddleware.ts
+++ app/backend/src/middlewares/ErrorMiddleware.ts
@@ -1,15 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { JsonWebTokenError } from 'jsonwebtoken';
+import Messages from '../schemas/Messages';
 import RequestError from '../utils/RequestError';
 
-type ErrType = RequestError;
+type ErrType = RequestError | JsonWebTokenError;
 
 class ErrorMiddleware {
   public static handle(err: ErrType, _req: Request, res: Response, _next: NextFunction) {
     let status = StatusCodes.INTERNAL_SERVER_ERROR;
-    let message = 'Internal Server Error';
+    let message: string = Messages.InternalServerError;
 
-    if (err.message && err.status) {
+    if (err instanceof JsonWebTokenError) {
+      status = StatusCodes.UNAUTHORIZED;
+      message = Messages.TokenInvalid;
+    }
+
+    if (err instanceof RequestError && !!(err.status && err.message)) {
       status = err.status;
       message = err.message;
     }
