0961fd1e3b71d3c6c945250f88c8a2a92b5415c7
Guilherme <guilhaugusto05@gmail.com>
2022-06-01 14:29:41 -0300

    refractor remove try/catch from LoginValidateController

--- app/backend/src/modules/Login/LoginValidateController.ts
+++ app/backend/src/modules/Login/LoginValidateController.ts
@@ -1,16 +1,12 @@
-import { NextFunction, Response } from 'express';
+import { Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { ReqWithUser } from '../../middlewares/Auth';
 
 class LoginValidateController {
-  public static handle = async (req: ReqWithUser, res: Response, next: NextFunction) => {
-    try {
-      const { user } = req;
+  public static handle = async (req: ReqWithUser, res: Response) => {
+    const { user } = req;
 
-      return res.status(StatusCodes.OK).json(user?.role);
-    } catch (error) {
-      next(error);
-    }
+    return res.status(StatusCodes.OK).json(user!.role);
   };
 }
 
