b3a5ca6334ad27a8b22bb594a602c9c90c1baf79
Guilherme <guilhaugusto05@gmail.com>
2022-06-01 20:28:34 -0300

    feat: create sequelize match repository

---
+++ app/backend/src/repositories/Match/SequelizeMatchRepository.ts
@@ -0,0 +1,25 @@
+import { IMatchRepository } from './IMatchRepository';
+import { IMatchWithTeams } from '../../interfaces/match';
+import Match from '../../database/models/Match';
+import Team from '../../database/models/Team';
+
+class SequelizeMatchRepository implements IMatchRepository {
+  private client: typeof Match;
+
+  constructor() {
+    this.client = Match;
+  }
+
+  findAll = async () => {
+    const matches = await this.client.findAll({
+      include: [
+        { model: Team, foreignKey: 'teamHome', as: 'teamHome', attributes: ['teamName'] },
+        { model: Team, foreignKey: 'teamAway', as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
+
+    return matches as unknown as IMatchWithTeams[];
+  };
+}
+
+export default SequelizeMatchRepository;
