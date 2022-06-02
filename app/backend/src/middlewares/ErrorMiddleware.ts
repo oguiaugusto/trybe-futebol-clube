e9b9b7e05b79ac7a644af7c2726d847a3790f79a
Guilherme <guilhaugusto05@gmail.com>
2022-06-02 14:21:27 -0300

    refractor: ErrorMiddleware to handle celebrate errors

--- app/backend/src/middlewares/ErrorMiddleware.ts
+++ app/backend/src/middlewares/ErrorMiddleware.ts
@@ -1,10 +1,12 @@
+import { CelebrateError, isCelebrateError } from 'celebrate';
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { JsonWebTokenError } from 'jsonwebtoken';
 import Messages from '../schemas/Messages';
 import RequestError from '../utils/RequestError';
 
-type ErrType = RequestError | JsonWebTokenError;
+type ErrType = RequestError | JsonWebTokenError | CelebrateError;
+type CelebrateMessage = { message: string };
 
 class ErrorMiddleware {
   public static handle(err: ErrType, _req: Request, res: Response, _next: NextFunction) {
@@ -14,6 +16,14 @@ class ErrorMiddleware {
     if (err instanceof JsonWebTokenError) {
       status = StatusCodes.UNAUTHORIZED;
       message = Messages.TokenInvalid;
+    }
+
+    if (isCelebrateError(err)) {
+      const { message: celebrateMessage }: CelebrateMessage = err
+        .details.entries().next().value[1].details[0];
+      message = celebrateMessage;
+
+      status = StatusCodes.BAD_REQUEST;
     }
 
     if (err instanceof RequestError && !!(err.status && err.message)) {
