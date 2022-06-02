52500221a7b336e498a8309bab1be8590b6b278c
Guilherme <guilhaugusto05@gmail.com>
2022-06-02 14:27:38 -0300

    feat: create endpoint POST /matches

--- app/backend/src/routes/matches.ts
+++ app/backend/src/routes/matches.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import { celebrate, Segments } from 'celebrate';
+import matchCreateController from '../modules/Match/Create';
 import matchFindAllController from '../modules/Match/FindAll';
+import Schemas from '../schemas/joi';
 
 const matchsRouter = Router();
 
 matchsRouter
   .route('/')
-  .get(matchFindAllController.handle);
+  .get(matchFindAllController.handle)
+  .post(
+    celebrate({ [Segments.BODY]: Schemas.matchPost }),
+    matchCreateController.handle,
+  );
 
 export default matchsRouter;
