8e2356a3f19ede8e2890f981dac7e2d7a4fb48ef
Guilherme <guilhaugusto05@gmail.com>
2022-06-02 14:26:03 -0300

    feat: add match create controller

---
+++ app/backend/src/modules/Match/Create/MatchCreateController.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { IMatchCreateService } from './MatchCreateService';
+
+class MatchCreateController {
+  constructor(private service: IMatchCreateService) {
+    this.service = service;
+  }
+
+  public handle = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = req.body;
+      const match = await this.service.handle({ homeTeam, awayTeam, homeTeamGoals, awayTeamGoals });
+
+      return res.status(StatusCodes.CREATED).json(match);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default MatchCreateController;
