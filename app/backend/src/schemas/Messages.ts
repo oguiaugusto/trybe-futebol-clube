cc2df98995400c70107fe5a41879e873dceee0c3
Guilherme <guilhaugusto05@gmail.com>
2022-06-02 21:11:50 -0300

    feat: add new messages

--- app/backend/src/schemas/Messages.ts
+++ app/backend/src/schemas/Messages.ts
@@ -5,8 +5,12 @@ enum Messages {
   TokenNotFound = 'Token not found',
   TokenInvalid = 'Invalid token',
   NoTokenUser = 'Error finding token user',
-  TeamNotFound = 'Team not found',
+  TeamNotFound = 'There is no team with such id!',
   TeamsMustBeRegistered = 'All teams must be registered',
+  MatchNotFound = 'There is no match with such id!',
+  MatchAlreadyEnded = 'Match is already finished',
+  Finished = 'Finished',
+  NoEqualTeams = 'It is not possible to create a match with two equal teams',
 }
 
 export default Messages;
