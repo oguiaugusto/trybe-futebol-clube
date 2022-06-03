3303e85f730be2764687db4e86b82a60ea6ff30b
Guilherme <guilhaugusto05@gmail.com>
2022-06-03 16:51:07 -0300

    chore: add mocks for sortLeaderboards

---
+++ app/backend/src/tests/mocks/sortLeaderboards.ts
@@ -0,0 +1,38 @@
+import { ILeaderboard } from '../../interfaces/leaderboard';
+
+const rawOtherInfos = { totalGames: 0, totalDraws: 0, totalLosses: 0, efficiency: 10.10 };
+
+const palmeiras: ILeaderboard = {
+  ...rawOtherInfos,
+  name: 'Palmeiras',
+  totalVictories: 5,
+  totalPoints: 10,
+  goalsBalance: 9,
+  goalsFavor: 10,
+  goalsOwn: 1,
+};
+const corinthians: ILeaderboard = {
+  ...rawOtherInfos,
+  name: 'Corinthians',
+  totalVictories: 3,
+  totalPoints: 8,
+  goalsBalance: 5,
+  goalsFavor: 8,
+  goalsOwn: 3,
+};
+const santos: ILeaderboard = {
+  ...rawOtherInfos,
+  name: 'Santos',
+  totalVictories: 2,
+  totalPoints: 7,
+  goalsBalance: 4,
+  goalsFavor: 6,
+  goalsOwn: 2,
+};
+
+export {
+  rawOtherInfos,
+  palmeiras,
+  corinthians,
+  santos,
+};
