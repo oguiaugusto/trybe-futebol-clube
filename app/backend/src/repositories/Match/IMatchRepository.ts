af3887fa6c8427d44541b2222e14ef6b7bb2e430
Guilherme <guilhaugusto05@gmail.com>
2022-06-02 21:09:03 -0300

    feat: add methods findById and finish to match repository

--- app/backend/src/repositories/Match/IMatchRepository.ts
+++ app/backend/src/repositories/Match/IMatchRepository.ts
@@ -1,7 +1,11 @@
+import Match from '../../database/models/Match';
 import { IMatch, IMatchDTO, IMatchWithTeams } from '../../interfaces/match';
 
 export interface IMatchRepository {
   findAll: () => Promise<IMatchWithTeams[]>;
   findAllByProgressCondition: (inProgress: boolean) => Promise<IMatchWithTeams[]>;
   create: (match: IMatchDTO) => Promise<IMatch>;
+  // findById returns Match instead of IMatch because i'll use this return on finish service
+  findById: (id: number) => Promise<Match | null>;
+  finish: (match: Match) => Promise<IMatch>;
 }
--- app/backend/src/repositories/Match/SequelizeMatchRepository.ts
+++ app/backend/src/repositories/Match/SequelizeMatchRepository.ts
@@ -33,12 +33,27 @@ class SequelizeMatchRepository implements IMatchRepository {
     return matches as unknown as IMatchWithTeams[];
   };
 
+  findById = async (id: number) => {
+    const match = await this.client.findByPk(id);
+    return match;
+  };
+
   create = async ({ homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress }: IMatchDTO) => {
     const match = await this.client.create({
       homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress,
     });
 
     return match;
+  };
+
+  finish = async (match: Match) => {
+    // I'm creating this helper because otherwise,
+    // i'll have to set this finish method as static =)
+    const helper = () => typeof this.client;
+    helper();
+
+    const endedMatch = await match.update({ inProgress: false });
+    return endedMatch;
   };
 }
 
