01be1f877725de3608f15d915ff25aab1ae889dd
Guilherme <guilhaugusto05@gmail.com>
2022-06-03 16:24:18 -0300

    feat: create sortLeaderboard util method and test it

---
+++ app/backend/src/tests/UtilsSortLeaderboards.test.ts
@@ -0,0 +1,56 @@
+import { expect } from 'chai';
+import { ILeaderboard } from '../interfaces/leaderboard';
+import { palmeiras, corinthians, santos } from './mocks/sortLeaderboards';
+import Utils from '../utils/Utils';
+
+describe('Test Utils.sortLeaderboard', () => {
+  it('should return sorted leaderboard by total points', () => {
+    const team1 = palmeiras;
+    const team2 = corinthians;
+    const team3 = santos;
+
+    const sortedLeaderboard = Utils.sortLeaderboard([team2, team3, team1]);
+
+    expect(sortedLeaderboard).to.be.eql([team1, team2, team3]);
+  });
+
+  it('should return sorted leaderboard by victories if total points are the same', () => {
+    const team1: ILeaderboard = { ...palmeiras, totalPoints: 10 };
+    const team2: ILeaderboard = { ...corinthians, totalPoints: 10 };
+    const team3: ILeaderboard = { ...santos, totalPoints: 10 };
+
+    const sortedLeaderboard = Utils.sortLeaderboard([team2, team3, team1]);
+
+    expect(sortedLeaderboard).to.be.eql([team1, team2, team3]);
+  });
+
+  it('should return sorted leaderboard by goals balance if total points and victories are the same', () => {
+    const team1: ILeaderboard = { ...palmeiras, totalPoints: 10, totalVictories: 5 };
+    const team2: ILeaderboard = { ...corinthians, totalPoints: 10, totalVictories: 5 };
+    const team3: ILeaderboard = { ...santos, totalPoints: 10, totalVictories: 5 };
+
+    const sortedLeaderboard = Utils.sortLeaderboard([team2, team3, team1]);
+
+    expect(sortedLeaderboard).to.be.eql([team1, team2, team3]);
+  });
+
+  it('should return sorted leaderboard by goals in favor if total points, victories and goals balance are the same', () => {
+    const team1: ILeaderboard = { ...palmeiras, totalPoints: 10, totalVictories: 5, goalsBalance: 9 };
+    const team2: ILeaderboard = { ...corinthians, totalPoints: 10, totalVictories: 5, goalsBalance: 9 };
+    const team3: ILeaderboard = { ...santos, totalPoints: 10, totalVictories: 5, goalsBalance: 9 };
+
+    const sortedLeaderboard = Utils.sortLeaderboard([team2, team3, team1]);
+
+    expect(sortedLeaderboard).to.be.eql([team1, team2, team3]);
+  });
+
+  it('should return sorted leaderboard by goals owned if total points, victories, goals balance and goals in favor are the same', () => {
+    const team1: ILeaderboard = { ...palmeiras, totalPoints: 10, totalVictories: 5, goalsBalance: 9, goalsOwn: 1 };
+    const team2: ILeaderboard = { ...corinthians, totalPoints: 10, totalVictories: 5, goalsBalance: 9, goalsOwn: 1 };
+    const team3: ILeaderboard = { ...santos, totalPoints: 10, totalVictories: 5, goalsBalance: 9, goalsOwn: 1 };
+
+    const sortedLeaderboard = Utils.sortLeaderboard([team2, team3, team1]);
+
+    expect(sortedLeaderboard).to.be.eql([team1, team2, team3]);
+  });
+});
--- app/backend/src/utils/Utils.ts
+++ app/backend/src/utils/Utils.ts
@@ -1,7 +1,30 @@
+import { ILeaderboard } from '../interfaces/leaderboard';
+
 class Utils {
   public static checkEmail(email: string) {
     const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/gim;
     return !!email.match(emailRegex);
+  }
+
+  public static sortLeaderboard(leaderboard: ILeaderboard[]) {
+    const sorted = leaderboard.sort((a, b) => {
+      if (a.totalPoints === b.totalPoints) {
+        if (a.totalVictories === b.totalVictories) {
+          if (a.goalsBalance === b.goalsBalance) {
+            if (a.goalsFavor === b.goalsFavor) {
+              return b.goalsOwn - a.goalsOwn;
+            }
+            return b.goalsFavor - a.goalsFavor;
+          }
+          return b.goalsBalance - a.goalsBalance;
+        }
+        return b.totalVictories - a.totalVictories;
+      }
+
+      return b.totalPoints - a.totalPoints;
+    });
+
+    return sorted;
   }
 }
 
