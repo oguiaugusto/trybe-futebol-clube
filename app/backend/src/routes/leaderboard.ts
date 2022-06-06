6072c99e53ea62bbfaa0425bf3e8d680d003a207
Guilherme <guilhaugusto05@gmail.com>
2022-06-06 12:54:30 -0300

    feat: create endpoint GET /leaderboard/away

--- app/backend/src/routes/leaderboard.ts
+++ app/backend/src/routes/leaderboard.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import leaderboardAwayController from '../modules/Leaderboard/Away';
 import leaderboardHomeController from '../modules/Leaderboard/Home';
 
 const leaderboardRouter = Router();
@@ -6,5 +7,9 @@ const leaderboardRouter = Router();
 leaderboardRouter
   .route('/home')
   .get(leaderboardHomeController.handle);
+
+leaderboardRouter
+  .route('/away')
+  .get(leaderboardAwayController.handle);
 
 export default leaderboardRouter;
