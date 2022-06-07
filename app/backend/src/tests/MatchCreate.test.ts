89bbbe797fc9297f793f292587ae1b67c929546e
Guilherme <guilhaugusto05@gmail.com>
2022-06-07 12:20:44 -0300

    fix(test): POST /matches mocks

--- app/backend/src/tests/MatchCreate.test.ts
+++ app/backend/src/tests/MatchCreate.test.ts
@@ -10,6 +10,9 @@ import { teamsMock } from './mocks/team';
 import Messages from '../schemas/Messages';
 import { StatusCodes } from 'http-status-codes';
 import Team from '../database/models/Team';
+import { Identifier } from 'sequelize/types';
+import { FindOptions } from 'sequelize/types';
+import { Model } from 'sequelize/types';
 
 chai.use(chaiHttp);
 
@@ -19,20 +22,19 @@ describe('Endpoint POST /matches', () => {
   const chaiHttpResponse = async (body?: string | object) => (
     chai.request(app).post('/matches').send(body)
   );
-  const mockedTeam = sinon.stub(Team, 'findByPk');
+  let mockedTeam: sinon.SinonStub<[identifier?: Identifier, options?: Omit<FindOptions<any>, "where">], Promise<Model<any, any> | null>>;
 
   describe('On success', () => {
     beforeEach(async () => {
-      // Sinon's not letting me stub 2 different promises of the same function
-      // so, let's pretend that it's returning 2 teams instead of one, =)
-      mockedTeam.onFirstCall().resolves(teamsMock[0] as Team);
-      mockedTeam.onSecondCall().resolves(teamsMock[1] as Team);
-
+      mockedTeam = sinon.stub(Team, 'findByPk');
       sinon.stub(Match, 'create').resolves(matchesMock[2] as Match);
+
+      mockedTeam.onCall(0).resolves(teamsMock[0] as Team);
+      mockedTeam.onCall(1).resolves(teamsMock[1] as Team);
     });
 
     afterEach(() => {
-      (Team.findByPk as sinon.SinonStub).restore();
+      mockedTeam.restore();
       (Match.create as sinon.SinonStub).restore();
     });
 
@@ -46,17 +48,18 @@ describe('Endpoint POST /matches', () => {
 
   describe('On fail', () => {
     beforeEach(async () => {
-      mockedTeam.onFirstCall().resolves(teamsMock[0] as Team);
-      mockedTeam.onSecondCall().resolves(teamsMock[1] as Team);
-
+      mockedTeam = sinon.stub(Team, 'findByPk');
       sinon.stub(Match, 'create').resolves(undefined);
     });
 
     afterEach(() => {
+      mockedTeam.restore();
       (Match.create as sinon.SinonStub).restore();
     });
 
     it('should return an error message if one of the teams does not exist', async () => {
+      mockedTeam.onCall(0).resolves(teamsMock[0] as Team);
+      mockedTeam.onCall(1).resolves(null);
       const response = await chaiHttpResponse(matchWithInvalidTeams);
 
       expect(response.status).to.be.equal(StatusCodes.NOT_FOUND);
@@ -65,10 +68,8 @@ describe('Endpoint POST /matches', () => {
     });
 
     it('should return an error message if both teams are the same', async () => {
-      mockedTeam.restore();
-      mockedTeam.onFirstCall().resolves(teamsMock[0] as Team);
-      mockedTeam.onSecondCall().resolves(teamsMock[0] as Team);
-
+      mockedTeam.onCall(0).resolves(teamsMock[0] as Team);
+      mockedTeam.onCall(1).resolves(teamsMock[0] as Team);
       const response = await chaiHttpResponse(matchWithSameTeam);
 
       expect(response.status).to.be.equal(StatusCodes.UNAUTHORIZED);
@@ -102,3 +103,4 @@ describe('Endpoint POST /matches', () => {
     });
   });
 });
+
